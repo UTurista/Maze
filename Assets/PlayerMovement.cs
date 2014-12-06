b538462fd839e8f244f543d1c46b0e633ec8333b
UTurista <vascko@sapo.pt>
2014-12-06 14:17:57 +0000

    Removed hardcoded values
    Values like: Speed and Map (to load) can be acess from inspector

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,22 +2,24 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	public float maxSpeed = 20F;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	//Caracter Speed
+	public float speed = 20F;
+
+	//Speed Multiplier if Button "Turbo" is being pressed
+	public float turboMultiplier = 3F;
+
+
 	// Update is called once per frame
 	void Update () {
-
-
+		//Speed multiplier, default 1
+		float multiplier =  Input.GetButton("Turbo") ? turboMultiplier : 1F;
+	
+		//Vertical movement
 		float xxMovment = Input.GetAxis ("Vertical");
-		rigidbody2D.velocity = new Vector3 (xxMovment * maxSpeed, rigidbody2D.velocity.y);	
+		rigidbody2D.velocity = new Vector3 (xxMovment * speed * multiplier, rigidbody2D.velocity.y);	
 
-
-
+		//Horizontal movement
 		float yyMovment = Input.GetAxis ("Horizontal");
-		rigidbody2D.velocity = new Vector3 (yyMovment * maxSpeed, rigidbody2D.velocity.x);	
+		rigidbody2D.velocity = new Vector3 (yyMovment * speed * multiplier, rigidbody2D.velocity.x);	
 	}
 }
