058147a88e0c3b96816dbacd4dd4c7eaef403c22
UTurista <vascko@sapo.pt>
2014-12-06 00:06:16 +0000

    FirstCommit

---
+++ Assets/Maze.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+
+public class Maze : MonoBehaviour {
+	public string mapName;
+	void Awake() {
+	{
+
+
+
+		
+		Debug.Log("Awake()");
+		// Load a single texture
+			Texture2D texture = Resources.Load(mapName) as Texture2D;
+		for (int xx=0; xx<texture.width; xx++) {
+			for (int yy=0;yy<texture.height;yy++){
+				Color color = texture.GetPixel(xx, yy);
+				//Debug.Log("Pixel["+xx+","+yy+"]="+color);
+				if(color==Color.black){
+					//Debug.Log("Create Cube");
+					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					cube.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
+					DestroyImmediate(cube.GetComponent("BoxCollider"));
+					cube.AddComponent("BoxCollider2D");
+		
+				}else if(color==Color.blue){
+						Debug.Log("Move Player");
+					GameObject caracter = GameObject.Find("Caracter");
+					caracter.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
+				}
+				else if(color==Color.white){
+					//Do Nothing
+				}else{
+						Debug.Log("Move Player");
+						GameObject caracter = GameObject.Find("Caracter");
+						caracter.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
+
+				}
+			} 
+		}
+	}
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float maxSpeed = 20F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		float xxMovment = Input.GetAxis ("Vertical");
+		rigidbody2D.velocity = new Vector3 (xxMovment * maxSpeed, rigidbody2D.velocity.y);	
+
+
+
+		float yyMovment = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector3 (yyMovment * maxSpeed, rigidbody2D.velocity.x);	
+	}
+}
