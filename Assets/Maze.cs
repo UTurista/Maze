1fb48ed333aa26c822b3e54aaf2f66e7f0c92ef4
UTurista <vascko@sapo.pt>
2014-12-06 15:00:07 +0000

    Added more values to Inspector
    Added Maze::startColor
    Added Maze::caracterName
    Added comments in code

--- Assets/Maze.cs
+++ Assets/Maze.cs
@@ -4,41 +4,46 @@
 
 public class Maze : MonoBehaviour {
 	public string mapName;
+	//Color that identifies were the caracter should start
+	public Color startColor=Color.blue;
+	//Name of the gameObject that is the Caracter
+	public string caracterName = "Caracter";
+	
+	
 	void Awake() {
-	{
-
-
-
-		
-		Debug.Log("Awake()");
 		// Load a single texture
-			Texture2D texture = Resources.Load(mapName) as Texture2D;
+		Texture2D texture = Resources.Load(mapName) as Texture2D;
+		
 		for (int xx=0; xx<texture.width; xx++) {
 			for (int yy=0;yy<texture.height;yy++){
+				//Get the color of the current Pixel
 				Color color = texture.GetPixel(xx, yy);
-				//Debug.Log("Pixel["+xx+","+yy+"]="+color);
+				
 				if(color==Color.black){
-					//Debug.Log("Create Cube");
 					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+					//Sets the Cube in the right position
 					cube.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
+					
+					//Replaces 3D Collision for a 2D one
 					DestroyImmediate(cube.GetComponent("BoxCollider"));
 					cube.AddComponent("BoxCollider2D");
 		
-				}else if(color==Color.blue){
-						Debug.Log("Move Player");
-					GameObject caracter = GameObject.Find("Caracter");
+				}else if(color==startColor){
+					//Special Color
+					GameObject caracter = GameObject.Find(caracterName);
 					caracter.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
 				}
 				else if(color==Color.white){
 					//Do Nothing
 				}else{
-						Debug.Log("Move Player");
-						GameObject caracter = GameObject.Find("Caracter");
-						caracter.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
+					//hack: Color.blue is not working
+					GameObject caracter = GameObject.Find(caracterName);
+					caracter.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
 
 				}
 			} 
 		}
 	}
-	}
 }
+
+
