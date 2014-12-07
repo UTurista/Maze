fc6ba934c55933040acd92a045751c4828f7538d
UTurista <vascko@sapo.pt>
2014-12-07 01:41:32 +0000

    Added an End to the Maze
    Added in Maze behaviour to add colliders that will trigger a (choosable)
    script
    Added EndMaze, a basic script that restarts the Level

---
+++ Assets/EndMaze.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndMaze : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Application.LoadLevel(0);
+	}
+}
--- Assets/Maze.cs
+++ Assets/Maze.cs
@@ -6,16 +6,22 @@ public class Maze : MonoBehaviour {
 	public string mapName;
 	//Color that identifies were the caracter should start
 	public Color startColor=Color.blue;
+	//Color that identifies were the caracter should start
+	public Color endColor=Color.red;
 	//Name of the gameObject that is the Caracter
 	public string caracterName = "Caracter";
+	//Behaviour to call when Caracter reaches Goal
+	public string endScriptName = "EndMaze";
 	
 	
 	void Awake() {
 		// Load a single texture
 		Texture2D texture = Resources.Load(mapName) as Texture2D;
 		
+		//We start to read the image from left to right, bottom to top
 		for (int xx=0; xx<texture.width; xx++) {
 			for (int yy=0;yy<texture.height;yy++){
+			
 				//Get the color of the current Pixel
 				Color color = texture.GetPixel(xx, yy);
 				
@@ -29,16 +35,32 @@ public class Maze : MonoBehaviour {
 					cube.AddComponent("BoxCollider2D");
 		
 				}else if(color==startColor){
+					Debug.Log ("Start at"+xx+","+yy);
+				
 					//Special Color
 					GameObject caracter = GameObject.Find(caracterName);
 					caracter.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
 				}
+				else if(color==endColor){
+					
+					GameObject endPos = new GameObject();
+					
+					//Sets the Cube in the right position
+					endPos.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
+					
+					//Adds a 2D collider
+					BoxCollider2D coll = endPos.AddComponent<BoxCollider2D>();
+					//if(coll==null){
+					//Debug.Log ("Is Null");}
+					coll.isTrigger = true;
+					
+					//Adds the end behaviour
+					endPos.AddComponent(endScriptName);
+				}
 				else if(color==Color.white){
 					//Do Nothing
 				}else{
-					//hack: Color.blue is not working
-					GameObject caracter = GameObject.Find(caracterName);
-					caracter.transform.position = new Vector3(xx-0.5F, yy-0.5F, 0);
+					Debug.Log("Other Color"+color.ToString());
 
 				}
 			} 
